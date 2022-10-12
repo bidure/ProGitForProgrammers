bed547c5511e9e89ed59e2a6a32a56f78d65e476
arusaki <abdellah.rejraji@gmail.com>
2022-10-12 17:33:11 +0100

    this is dudu commit

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
 Console.WriteLine("and this is a new line");
 Console.WriteLine("here's another one");
+Console.WriteLine("awah");
